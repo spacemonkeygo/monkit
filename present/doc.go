1a5011ab6d5bbe1723d8bed52287b280aa2101de
Jeff Wendling <jeff@spacemonkey.com>
2016-06-24 12:26:24 -0600

    present: fix import path restriction
    Change-Id: I8c5fabcf4ff55a96ac4869f006f6f57c90bcfdd7

--- present/doc.go
+++ present/doc.go
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package present // import "gopkg.in/spacemonkeygo/monkit.v2/environment"
+package present // import "gopkg.in/spacemonkeygo/monkit.v2/present"
