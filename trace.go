89e8fea30eab76dc7f4cd46e43519c8e7f17a713
Yingrong Zhao <yingrong.zhao@gmail.com>
2020-05-13 23:28:46 -0400

    copy the vals from existing trace

--- trace.go
+++ trace.go
@@ -142,9 +142,10 @@ func (t *Trace) Set(key, val interface{}) {
 
 // copyFrom replace all key/value on a trace with a new sets of key/value.
 func (t *Trace) copyFrom(s *Trace) {
+	vals := s.GetAll()
 	t.mtx.Lock()
 	defer t.mtx.Unlock()
-	t.vals = s.vals
+	t.vals = vals
 }
 
 func (t *Trace) incrementSpans() { atomic.AddInt64(&t.spanCount, 1) }
